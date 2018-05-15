74b7b7258142f8bbad58dd02c2d9f8e55d972fd1
Michael Dillie <michaeldillie@gmail.com>
2018-05-15 09:24:44 -0500

    Adding a few comments to letter.js. Also required word.js to index.js and required letter.js to word.js

--- letter.js
+++ letter.js
@@ -1,13 +1,21 @@
 var inquirer = require('inquirer');
 
+
+
+
 function Letter(letter) {
     this.isGuessed = false;
     this.letter = letter;
 
+    // This will check if the letter guessed
+    // has already been guessed
+    // If it has not been guessed that letter will
+    // be set to true
     this.checkLetter = function(letter) {
 
     }
 
+    // This will display each letter
     this.display = function() {
         if(!this.isGuessed) {
             console.log(this.letter);
